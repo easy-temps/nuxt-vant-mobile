1e2dd9bef7faba3668843624c34f68ab6be86855
CharleeWa <18888351756@163.com>
2024-12-30 18:48:48 +0800

    refactor: update fetch page

--- app/pages/prose/index.vue
+++ app/pages/prose/index.vue
@@ -9,15 +9,34 @@ definePageMeta({
 
 const proseStore = useProseStore()
 
-async function fetchData() {
-  await proseStore.fetchProse()
+function fetch() {
+  proseStore.fetchProse()
 }
 
-fetchData()
+function clear() {
+  proseStore.clearProse()
+}
 </script>
 
 <template>
   <div>
-    {{ proseStore.prose }}
+    <div class="h-300 flex items-center justify-center rounded-15 bg-white p-16 dark:bg-[--van-background-2]">
+      <div v-if="proseStore.prose" class="text-16 leading-26">
+        {{ proseStore.prose }}
+      </div>
+
+      <ClientOnly v-else>
+        <van-empty :description="$t('prose_page.btn_empty_desc')" />
+      </ClientOnly>
+    </div>
+
+    <van-space class="m-10" direction="vertical" fill>
+      <van-button type="primary" round block @click="fetch">
+        {{ $t('prose_page.btn_fetch') }}
+      </van-button>
+      <van-button type="default" round block @click="clear">
+        {{ $t('prose_page.btn_clear') }}
+      </van-button>
+    </van-space>
   </div>
 </template>
