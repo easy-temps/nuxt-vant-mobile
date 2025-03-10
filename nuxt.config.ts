22d83756e3466b75367d460f2aa3df3a35d6a10d
CharleeWa <18888351756@163.com>
2025-03-10 21:32:12 +0800

    fix: update postcss config to fix router scroll behavior
    ref: easy-temps/vue3-vant-mobile#145

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,8 +38,11 @@ export default defineNuxtConfig({
         maxDisplayWidth: 600,
         // devtools excluded
         exclude: /@nuxt/,
-        appContainingBlock: 'auto',
         border: true,
+        rootContainingBlockSelectorList: [
+          'van-tabbar',
+          'van-popup',
+        ],
       },
     },
   },
